5783dae4e1d3c46183f1d87e163c7121562e5b28
Matthieu Schulz <m.schulz18@ejm.org>
2020-10-17 16:55:41 +0100

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,8 @@
 import React from 'react'
 import {BrowerRouter as Router, Route} from 'react-router-dom';
 
-function App() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
-
-export default App
+const App = () => (
+    <Router>
+        <Route path="/" exact comp
+    </Router>
+);
