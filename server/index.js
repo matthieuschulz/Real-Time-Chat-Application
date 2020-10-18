6cbd895ea0a9da4f09df6bf0b3fb3b7555bce927
Matthieu Schulz <m.schulz18@ejm.org>
2020-10-18 14:52:42 +0100

    set up server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const PORT = process.env.PORT || 5000; // server will later require specific port
+
+const router = require('./router');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+app.use(router);
+
+server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
