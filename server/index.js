319a90207c73d926d0a42886be6318e4403a85ad
Matthieu Schulz <m.schulz18@ejm.org>
2020-10-18 20:02:38 +0100

    update

--- server/index.js
+++ server/index.js
@@ -10,6 +10,11 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+io.on('connection', => {
+    //setting up socket.io real time chat
+})
+
+
 app.use(router);
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
