91fb0202720cf29df9c9181e7d80fcfb4a3d8e80
Matthieu Schulz <m.schulz18@ejm.org>
2020-10-21 03:45:41 +0100

    updated chat.js

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,12 +1,18 @@
-import React,  {useState, useEffect} from 'react';
+import React,  {useState, useEffect} from 'react'; //useeffect is hook that helps us use side effects, it happens on component deadmount
 import queryString from 'query-string';// helps us to retrieve data from the url
 import io from 'socket.io-client';
 
 import './Chat.css';
 
 
-const Chat = () => {
-    return (
+const Chat = ({location}) => {
+
+    useEffect(() => {
+        const {name,room }  = queryString.parse(location.search);
+
+        console.log(name, room);
+    })
+    return ( 
         <h1>Chat</h1>
     )
 }
